2c7327f26e21ce944352b36682957284606595a1
Hardik gaur <114279965+Hardikgaur21@users.noreply.github.com>
2025-04-06 20:46:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,12 @@ resource "azurerm_linux_web_app" "this" {
   resource_group_name = azurerm_resource_group.this.name
   service_plan_id     = azurerm_service_plan.this.id
 
+  site_config {
+    application_stack {
+      node_version = "18-lts" # Use the Node.js version compatible with your React app
+    }
+  }
+
   identity {
     type = "SystemAssigned"
   }
